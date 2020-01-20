082f58c5968f25938a1006e91e3a3af344b2d95f
Dragana <distant.countries@gmail.com>
2020-01-20 16:23:36 +0100

    CSS iteration2

--- src/template/Footer.vue
+++ src/template/Footer.vue
@@ -5,7 +5,7 @@
                 <div class="border"></div>
             </div>
             <div class="footer-sections">
-                <div class="section">
+                <div class="section-links section">
                     <h1>Links</h1>
                     <a href="#"><h2>Company</h2></a>
                     <a href="#"><h2><span>Catalogs</span></h2></a>
@@ -16,29 +16,35 @@
                     <a href="#"><p>Login/Register</p></a>
                 </div>
                 <div class="footer-border"></div>
-                <div class="section">
-                    <h1>Dev test</h1>
-                    <p>
-                        Dev test<br />
-                        1421 38th Brooklyn, NY<br />
-                        11245-5647 USA<br /><br />
+                <div class="section-adress section">
+                    <h1>Dev Test</h1>
+                    <p class="adress">
+                        Dev Test<br />
+                        1421 38<sup>th</sup> Brooklyn, NY<br />
+                        11245-5647 USA
+                    </p>
+                    <p class="adress">
                         (718= 633-2300<br />
                         info@devtest.com
                     </p>
                 </div>
                 <div class="footer-border"></div>
-                <div class="section">
-                    <h1>Newsletter</h1>
-                    <div class="section-button-wrapper">
-                        <div class="section-button">
-                            <div class="icon segment">
-                                <img src="./../../public/img/icons/letter.png" />
-                            </div>
-                            <div class="text segment">
-                                <p>Email Address</p>
-                            </div>
-                            <div class="icon segment">
-                                <button @click="handleEmail()"><img src="./../../public/img/icons/arrow-right.png" /></button>
+                <div class="section-newsletter-wrapper1 section">
+                    <div class="section-newsletter-wrapper2">
+                        <div class="section-newsletter">
+                            <h1>Newsletter</h1>
+                            <div class="section-button-wrapper">
+                                <div class="section-button">
+                                    <div class="icon segment icon-letter">
+                                        <img src="./../../public/img/icons/letter.png" />
+                                    </div>
+                                    <div class="text segment">
+                                        <p>Email Address</p>
+                                    </div>
+                                    <div class="icon segment icon-arrow">
+                                        <button @click="handleEmail()"><img src="./../../public/img/icons/arrow-right.png" /></button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
